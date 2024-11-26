3f7c24679b9f4171375dba012b16c137c3e3acb8
Yuri de Oliveira Bezerra <yuri.snke@gmail.com>
2024-11-25 22:25:32 -0300

    Removido caracteres inválidos

--- src/controllers/investimentoController.js
+++ src/controllers/investimentoController.js
@@ -1,44 +1,47 @@
 import {
-    buscaInvestimentos,
-    criarInvestimentoRep
-  } from "../data/repositories/investimentoRepository.js";
+  buscaInvestimentos,
+  criarInvestimentoRep,
+} from "../data/repositories/investimentoRepository.js";
 import { Investimento } from "../models/investimentoModel.js";
-  
-  export const obterInvestimentos = async (req, res) => {
-    try {
-      const investimentosData = await buscaInvestimentos(
-        req.usuario.userId
-      );
-      if (investimentosData.length > 0) {
-        const investimentos = investimentosData.map(
-          (investimento) => new Investimento(investimento)
-        );
-        res.send(investimentos);
-      } else {
-        res.status(404).send({ message: "Nenhum resultado encontrado" });
-      }
-    } catch (err) {
-      res.status(500).send({ error: err.message });
-    }
-  };
 
-  export const criarInvestimento = async (req, res) => {
-    try {s
-      const investimento = new Investimento({
-        ...req.body,
-        usuario_id: req.usuario.userId,
-        valor_investido: isNaN(parseFloat(req.body.valor_investido)) ? 0 : parseFloat(req.body.valor_investido), 
-        rendimento_percentual_anual: isNaN(parseFloat(req.body.rendimento_percentual_anual)) ? 0 : parseFloat(req.body.rendimento_percentual_anual),
-      });
-      s
-      const investimentoId = await criarInvestimentoRep (investimento);
-  
-      res.send({
-        message: "Investimento criado com sucesso!",
-        id: investimentoId,
-      });
-    } catch (err) {
-      res.status(500).send({ error: err.message });
+export const obterInvestimentos = async (req, res) => {
+  try {
+    const investimentosData = await buscaInvestimentos(req.usuario.userId);
+    if (investimentosData.length > 0) {
+      const investimentos = investimentosData.map(
+        (investimento) => new Investimento(investimento)
+      );
+      res.send(investimentos);
+    } else {
+      res.status(404).send({ message: "Nenhum resultado encontrado" });
     }
-  };
-  
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+export const criarInvestimento = async (req, res) => {
+  try {
+    const investimento = new Investimento({
+      ...req.body,
+      usuario_id: req.usuario.userId,
+      valor_investido: isNaN(parseFloat(req.body.valor_investido))
+        ? 0
+        : parseFloat(req.body.valor_investido),
+      rendimento_percentual_anual: isNaN(
+        parseFloat(req.body.rendimento_percentual_anual)
+      )
+        ? 0
+        : parseFloat(req.body.rendimento_percentual_anual),
+    });
+
+    const investimentoId = await criarInvestimentoRep(investimento);
+
+    res.send({
+      message: "Investimento criado com sucesso!",
+      id: investimentoId,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
